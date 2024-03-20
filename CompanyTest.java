ecd9d1e2c56652384305bb256a79f5eb52187e9f
Manuel Cantante <202002021@estudantes.ips.pt>
2024-03-20 10:59:17 +0000

    Nível 3:

---
+++ CompanyTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+public class CompanyTest {
+
+    private Company company;
+    private User client1, client2, seller1, seller2;
+
+    @Before
+    public void setUp() {
+        company = new Company();
+        client1 = new User("José Manuel", "911111111", "zemanel@yahoo.com");
+        client2 = new User("António Francisco", "922222222", "tochico@hotmail.com");
+        seller1 = new User("Fernando Fernandes", "966777101", "fefe@remax.pt");
+        seller2 = new User("Rodrigo Rodrigues", "966777152", "roro@remax.pt");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(company.getClients());
+        assertNotNull(company.getSellers());
+        assertNotNull(company.getProperties());
+        assertNotNull(company.getSells());
+    }
+}
